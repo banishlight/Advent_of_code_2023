aff44651c3f8d04e17eb21234b857aee33ea04ef
Cuba Giesbrecht <cug429@usask.ca>
2023-12-06 10:51:25 +0000

    Further implementation of Day 4

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -1,6 +1,43 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int checkForWinners(vector<int> winners, vector<int> chosen) {
+    int result = 0;
+    unordered_set<int> winnerSet(winners.begin(), winners.end());
+
+    for (int num : chosen) {
+        if (winnerSet.find(num) != winnerSet.end()) {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+vector<int> readWinners() {
+
+}
+
+vector<int> readChosen() {
+
+}
+
+void solveDay4() {
+    vector<int> winners;
+    vector<int> chosen;
+}
 
 
 
 int main() {
+    // Line by line:
+        // Read up to ':'
+        // then collect the next 10 1-2 digit numbers, we will call these "winners"
+        // then collect the chosen numbers "chosen"
+        // Iterate over chosen numbers and see if they appear in the winners list
+    solveDay4();
     
+
 }
